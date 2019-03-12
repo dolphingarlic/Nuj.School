78662d1441a3b72c731a45d4af0f613d07475ad5
AnqiQu <anqiqu@gmail.com>
2019-03-12 14:13:17 +0200

    Polishing up the screens

---
+++ app/Button.java
@@ -0,0 +1,10 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Button{
+
+    Button()
+        }
